d8637e28b5a35f266d0cc6eea72b3f9dca9c6ff6
alina_tomiuk <tomukalina9@gmail.com>
2026-05-25 17:25:40 +0200

    change task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,14 @@
 function calcAverageCalories(days) {
+  if (days.length === 0) {
+    return 0;
+  }
+
   let total = 0;
+
   for (const day of days) {
     total += day.calories;
   }
+
   return total / days.length;
 }
 
